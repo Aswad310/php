aa3ae9bc0a4b6d270d4075f33464babb3e9d198a
Aswad310 <aswadali310@gmail>
2022-09-24 17:31:12 +0500

    == vs ===

--- equality-identity.php
+++ equality-identity.php
@@ -1,17 +1,15 @@
 <?php
-    $value = 123;
-
 ############ Equality  ############
-    /* (only value will be true) */
+/* (true if $x is equal to $y after type juggling.) */
     echo "Equality == <br>";
-    if($value == 123.0){
+    if(123 == 123.0){
         echo "True {$value}<hr>";
     }
 
 ############ Identity  ############
-    /* (Value and data type must be true) */
+    /* (true if $a is equal to $b, and they are of the same type.) */
     echo "Identity === <br>";
-    if($value === 123.0){
+    if(123 === 123.0){
         echo "True {$value}";
     }
 ?>
