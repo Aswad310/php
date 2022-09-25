4b78be5a71c8f14bf504a9458c76c5473aabb4f8
Aswad310 <aswadali310@gmail>
2022-09-25 22:02:44 +0500

    Dynamic Page Title

--- index.php
+++ index.php
@@ -15,8 +15,28 @@ include('includes/ViewUser.inc.php');
   
   <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-        <div class="">
-            <img src="purelogics-logo.jpeg" alt="PL-Logo" style="width: 70px;">
-        </div>
+        <section>   
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid ms-4">
+                    <div>
+                        <a class="navbar-brand" href="#">
+                            <img src="pl-logo.jpeg" alt="Logo" width="50" class="d-inline-block align-text-top">
+                        </a>
+                    </div>
+                    
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+                        <a class="nav-link" href="./math_tables.php">Math Tables</a>
+                        <a class="nav-link" href="./output_buffering.php">About</a>
+                        <a class="nav-link" href="#">Contact</a>
+                    </div>
+                    </div>
+                </div>
+            </nav>
+        </section>
     </body>
 </html>
