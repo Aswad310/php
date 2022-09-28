926a143a4c9fecb9691db03fd3f9a78fadf6e6fb
Aswad310 <aswadali310@gmail>
2022-09-28 14:05:48 +0500

    contact-us

---
+++ contact-us.php
@@ -0,0 +1,53 @@
+<?php include('./index.php') ?>
+
+<div class="row ms-5 me-5 pt-3">
+    <div class="col ms-5 me-5">
+        <div class="col ms-5">
+            <h1>Get in touch, send us an inquiry.</h1>
+        </div>
+    </div>    
+</div>
+
+<div class="ms-5 me-5">
+    <div class="ms-5 me-5">
+        <form class="row g-3 m-5 ">
+        
+        <div class="col-md-6">
+            <label for="inputEmail4" class="form-label">Email</label>
+            <input type="email" class="form-control" id="inputEmail4">
+        </div>
+
+        <div class="col-md-6">
+            <label for="inputname4" class="form-label">Name</label>
+            <input type="test" class="form-control" id="inputname4">
+        </div>
+        
+        <div class="col-12">
+            <label for="inputAddress" class="form-label">Address</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+        </div>
+        
+        <div class="col-md-6">
+            <label for="inputCity" class="form-label">City</label>
+            <input type="text" class="form-control" id="inputCity">
+        </div>
+        
+        <div class="col-md-4">
+            <label for="inputState" class="form-label">State</label>
+            <select id="inputState" class="form-select">
+            <option selected>Choose...</option>
+            <option>...</option>
+            </select>
+        </div>
+        
+        <div class="col-md-2">
+            <label for="inputZip" class="form-label">Zip</label>
+            <input type="text" class="form-control" id="inputZip">
+        </div>
+        
+        <div class="col-12 pt-4">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+        </form>
+    </div>    
+</div>
