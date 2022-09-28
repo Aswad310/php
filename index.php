87b43ba65dd4c6a1739b170bf89b158fcde49334
Aswad310 <aswadali310@gmail>
2022-09-28 14:09:38 +0500

    Dynamic Page Title

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php
-include('includes/Dbh.inc.php');
-include('includes/User.inc.php');
-include('includes/ViewUser.inc.php');
-?>
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -25,15 +19,16 @@ include('includes/ViewUser.inc.php');
                     </div>
                     
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"></span>
                     </button>
+                    
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
-                        <a class="nav-link" href="./math_tables.php">Math Tables</a>
-                        <a class="nav-link" href="./output_buffering.php">About</a>
-                        <a class="nav-link" href="#">Contact</a>
-                    </div>
+                        <div class="navbar-nav">
+                            <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+                            <a class="nav-link" href="./math_tables.php">Math Tables</a>
+                            <a class="nav-link" href="./output_buffering.php">About</a>
+                            <a class="nav-link" href="./contact-us.php">Contact</a>
+                        </div>
                     </div>
                 </div>
             </nav>
