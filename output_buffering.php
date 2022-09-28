21fdd7c0b9b3b3bfc41816073ba399e36c0d86a5
Aswad310 <aswadali310@gmail>
2022-09-28 14:10:59 +0500

    Output Buffering

--- output_buffering.php
+++ output_buffering.php
@@ -8,22 +8,24 @@
     echo $nav;
 ?>
 
-<div class="row m-4 p-4" style="text-align: justify;">
-    <div class="col-4 ">
-        <h2>Coloumn 1</h2>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et veniam odit nihil velit in aut magnam aspernatur veritatis vitae. Qui quos maxime cumque amet sed animi culpa quia. Pariatur, suscipit.
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quia, dolores magni, eveniet quam unde sunt iusto porro similique temporibus nisi sed! Sapiente, eaque quod non commodi repudiandae dicta placeat.
-    </div>
+<div class="m-5">
+    <div class="row ms-4 me-4 p-4" style="text-align: justify;">
+        <div class="col-4 ">
+            <h2>Coloumn 1</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et veniam odit nihil velit in aut magnam aspernatur veritatis vitae. Qui quos maxime cumque amet sed animi culpa quia. Pariatur, suscipit.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quia, dolores magni, eveniet quam unde sunt iusto porro similique temporibus nisi sed! Sapiente, eaque quod non commodi repudiandae dicta placeat.
+        </div>
 
-    <div class="col-4 ">
-        <h2>Coloumn 2</h2>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et veniam odit nihil velit in aut magnam aspernatur veritatis vitae. Qui quos maxime cumque amet sed animi culpa quia. Pariatur, suscipit.
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quia, dolores magni, eveniet quam unde sunt iusto porro similique temporibus nisi sed! Sapiente, eaque quod non commodi repudiandae dicta placeat.
-    </div>
+        <div class="col-4 ">
+            <h2>Coloumn 2</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et veniam odit nihil velit in aut magnam aspernatur veritatis vitae. Qui quos maxime cumque amet sed animi culpa quia. Pariatur, suscipit.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quia, dolores magni, eveniet quam unde sunt iusto porro similique temporibus nisi sed! Sapiente, eaque quod non commodi repudiandae dicta placeat.
+        </div>
 
-    <div class="col-4 ">
-        <h2>Coloumn 3</h2>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et veniam odit nihil velit in aut magnam aspernatur veritatis vitae. Qui quos maxime cumque amet sed animi culpa quia. Pariatur, suscipit.
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quia, dolores magni, eveniet quam unde sunt iusto porro similique temporibus nisi sed! Sapiente, eaque quod non commodi repudiandae dicta placeat.
+        <div class="col-4 ">
+            <h2>Coloumn 3</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et veniam odit nihil velit in aut magnam aspernatur veritatis vitae. Qui quos maxime cumque amet sed animi culpa quia. Pariatur, suscipit.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi quia, dolores magni, eveniet quam unde sunt iusto porro similique temporibus nisi sed! Sapiente, eaque quod non commodi repudiandae dicta placeat.
+        </div>
     </div>
 </div>
